bf47d0758d693eb3c10b49f89a0338a7a6280788
Mikael Koskinen <mikael.koskinen@adafy.com>
2021-05-19 09:51:33 +0300

    Moved form System data from Microsoft data

--- src/Weikio.ApiFramework.Plugins.SqlServer/SqlServerConnectionCreator.cs
+++ src/Weikio.ApiFramework.Plugins.SqlServer/SqlServerConnectionCreator.cs
@@ -1,5 +1,5 @@
 ﻿using System.Data.Common;
-using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
 using Weikio.ApiFramework.SDK.DatabasePlugin;
 
 namespace Weikio.ApiFramework.Plugins.SqlServer
