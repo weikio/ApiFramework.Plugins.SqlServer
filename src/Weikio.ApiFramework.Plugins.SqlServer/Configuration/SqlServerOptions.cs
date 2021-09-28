58c160680dcc9653b82e0469691994c28e3099a7
Mikael Koskinen <mikael.koskinen@adafy.com>
2021-09-28 19:08:19 +0300

    Updated to work with the DB SDK 2.0

--- src/Weikio.ApiFramework.Plugins.SqlServer/Configuration/SqlServerOptions.cs
+++ src/Weikio.ApiFramework.Plugins.SqlServer/Configuration/SqlServerOptions.cs
@@ -1,8 +1,22 @@
+using System.Data.Common;
+using Microsoft.Data.SqlClient;
+using SqlKata.Compilers;
 using Weikio.ApiFramework.SDK.DatabasePlugin;
 
 namespace Weikio.ApiFramework.Plugins.SqlServer.Configuration
 {
     public class SqlServerOptions : DatabaseOptionsBase
     {
+        public override DbConnection CreateConnection()
+        {
+            var result = new SqlConnection(ConnectionString);
+
+            return result;
+        }
+
+        public override Compiler CreateCompiler()
+        {
+            return new SqlServerCompiler() { UseLegacyPagination = false };
+        }
     }
 }
